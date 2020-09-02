584c7212c732bc354df0c93091ee05caebb7c66a
Alexander Liu <liujalexander84@gmail.com>
2020-09-02 09:24:09 -0700

    hhh

---
+++ README.md
@@ -0,0 +1,2 @@
+# sani
+Helloooooooooooo
--- src/bot.ts
+++ src/bot.ts
@@ -1,5 +1,5 @@
 export default class Bot {
-   static hallo(name?: string) {
+   static hallo(name?: string): void {
       if (!name) console.log("Hello!");
       else console.log(`Hello ${name}`);
    }
