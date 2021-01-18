41c8e38a864a2d8a6ae90b78d8915e02cdc6978b
Alexander Liu <liujalexander84@gmail.com>
2021-01-18 15:31:16 -0800

    delet uwu
    lol

--- src/ids.ts
+++ src/ids.ts
@@ -1,62 +1,11 @@
-// do i still need this not found string lol? idk
-// export const NOT_FOUND_STRING = "not found lol";
-
-// galacon role ids
-/** id for galacon 2012 role */
-export const y2012: string = "628136070642401280";
-/** id for galacon 2013 role */
-export const y2013: string = "628136127643123732";
-/** id for galacon 2014 role */
-export const y2014: string = "628136132701454346";
-/** id for galacon 2015 role */
-export const y2015: string = "628136394023370752";
-/** id for galacon 2016 role */
-export const y2016: string = "628136665721864202";
-/** id for galacon 2017 role */
-export const y2017: string = "628137097244442627";
-/** id for galacon 2018 role */
-export const y2018: string = "628136798299750421";
-/** id for galacon 2019 role */
-export const y2019: string = "628136797095854100";
-
-/** id for galacon artist role */
-export const artist: string = "605726501924765706";
-/** id for galacon musician role */
-export const musician: string = "605451090430918752";
-/** id for galacon cosplayer role */
-export const cosplayer: string = "607227135756599326";
-/** id for galacon meme role */
-export const meme: string = "605454893620396060";
-/** id for galacon roleplay role */
-export const rp: string = "755487852720291851";
-
 /** id for galacon #server-support */
 export const serversupportchannel: string = "613024393425059981";
 
 /** id for galacon mlem emoji */
 export const mlememoji: string = ":Mlem:758757084299788298";
 
-
-// development role ids in autumn's private server thing
-// people who develop on sani, feel free to add here if you want
-/*
-export const y2012: string = "740013333184381020";
-export const y2013: string = "740013318751911979";
-export const y2014: string = "740013297046257775";
-export const y2015: string = "740013284824055898";
-export const y2016: string = "740013272077434973";
-export const y2017: string = "740013259297652816";
-export const y2018: string = "740013246962073661";
-export const y2019: string = "740013228876365855";
-
-export const artist: string = "755287911338868768";
-export const musician: string = "755287986991792229";
-export const cosplayer: string = "755288011721408612";
-export const meme: string = "755288031724044311";
-export const rp: string = "757007891109642392";
-
-export const serversupportchannel: string = "578740813912211457";
-*/
+//
+// export const serversupportchannel: string = "578740813912211457";
 
 /**
  * id for the author of the bot (person to ping when something goes horribly wrong)
