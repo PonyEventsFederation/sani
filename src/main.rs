28af00a647d1bb43796460840b163329b70465a3
Autumn <blazeykirin@gmail.com>
2021-10-09 18:31:32 -0700

    tokio
    read the println! statement, it is true

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use tokio::runtime::Builder as TokioRuntimeBuilder;
+use std::time::Duration;
+
 fn main() {
-	println!("Hello, world!");
+	TokioRuntimeBuilder::new_multi_thread()
+		.enable_all()
+		.worker_threads(2)
+		.max_blocking_threads(32)
+		.thread_keep_alive(Duration::from_secs(60))
+		.build()
+		.unwrap()
+		.block_on(async_main());
+}
+
+async fn async_main() {
+	println!("i spent like 4 hours on this, help");
 }
