ad2b9547119b7f9d67de1981360b264d8471657f
Autumn Meadow <blazeykirin@gmail.com>
2021-11-05 01:12:03 +0000

    conditional compile modules for development/production

--- src/main.rs
+++ src/main.rs
@@ -56,19 +56,7 @@ async fn async_main() -> MainResult {
 
 	let HttpAndCluster { http, cluster, mut events } = setup_http_and_cluster(&env).await?;
 
-	let mut modules: Vec<Box<dyn modules::Module>> = vec![
-		Box::new(Status()),
-		Box::new(ReactionRole {
-			role_id: role_id(905_52966_33953_52587),
-			guild_id: guild_id(834_29759_11599_80073),
-			channel_id: channel_id(834_29852_87413_26869),
-			message_id: message_id(905_53005_37717_88360),
-			emoji: RequestReactionType::Custom {
-				id: emoji_id(897_60539_74504_61215),
-				name: None
-			}
-		})
-	];
+	let mut modules: Vec<Box<dyn modules::Module>> = modules();
 
 	let stuff = InitStuff {
 		http: Arc::clone(&http)
@@ -118,6 +106,7 @@ struct HttpAndCluster {
 	events: Events
 }
 
+#[inline]
 async fn setup_http_and_cluster(env: &Env) -> MainResult<HttpAndCluster> {
 	let http = HttpClient::new(env.token().to_string());
 	let http = Arc::new(http);
@@ -130,6 +119,215 @@ async fn setup_http_and_cluster(env: &Env) -> MainResult<HttpAndCluster> {
 	let cluster = Arc::new(cluster);
 
 	Ok(HttpAndCluster { http, cluster, events })
+}
+
+#[inline]
+#[cfg(debug_assertions)]
+fn modules() -> Vec<Box<dyn modules::Module>> {
+	vec![
+		Box::new(Status()),
+		Box::new(ReactionRole {
+			role_id: role_id(905_52966_33953_52587),
+			guild_id: guild_id(834_29759_11599_80073),
+			channel_id: channel_id(834_29852_87413_26869),
+			message_id: message_id(905_53005_37717_88360),
+			emoji: RequestReactionType::Custom {
+				id: emoji_id(897_60539_74504_61215),
+				name: None
+			}
+		})
+	]
+}
+
+#[inline]
+#[cfg(not(debug_assertions))]
+fn modules() -> Vec<Box<dyn modules::Module>> {
+	let guild_id = guild_id(602434888880095242);
+	let channel_id = channel_id(823351439279259648);
+	let server_role_message = message_id(839966718071406652);
+	let year_role_message = message_id(839966718507221013);
+
+	vec![
+		// artist
+		Box::new(ReactionRole {
+			channel_id,
+			guild_id,
+			emoji: RequestReactionType::Custom {
+				id: emoji_id(823502121487958016),
+				name: None
+			}, // :Artist:
+			message_id: server_role_message,
+			role_id: role_id(605726501924765706)
+		}),
+
+		// musician
+		Box::new(ReactionRole {
+			channel_id,
+			guild_id,
+			emoji: RequestReactionType::Custom {
+				id: emoji_id(823503291027161128),
+				name: None
+			}, // :DJ:
+			message_id: server_role_message,
+			role_id: role_id(605451090430918752)
+		}),
+
+		// cosplayer
+		Box::new(ReactionRole {
+			channel_id,
+			guild_id,
+			emoji: RequestReactionType::Custom {
+				id: emoji_id(639189237400469517),
+				name: None
+			}, // :Innkeep:
+			message_id: server_role_message,
+			role_id: role_id(607227135756599326)
+		}),
+
+		// meme
+		Box::new(ReactionRole {
+			channel_id,
+			guild_id,
+			emoji: RequestReactionType::Custom {
+				id: emoji_id(745824076940968077),
+				name: None
+			}, // :Dab:
+			message_id: server_role_message,
+			role_id: role_id(605454893620396060)
+		}),
+
+		// rp
+		Box::new(ReactionRole {
+			channel_id,
+			guild_id,
+			emoji: RequestReactionType::Custom {
+				id: emoji_id(667481831293190174),
+				name: None
+			}, // :Salute:
+			message_id: server_role_message,
+			role_id: role_id(755487852720291851)
+		}),
+
+		// news
+		Box::new(ReactionRole {
+			channel_id,
+			guild_id,
+			emoji: RequestReactionType::Custom {
+				id: emoji_id(823482395202158633),
+				name: None
+			}, // :Scroll
+			message_id: server_role_message,
+			role_id: role_id(784873956363599912)
+		}),
+
+		// movie night
+		Box::new(ReactionRole {
+			channel_id,
+			guild_id,
+			emoji: RequestReactionType::Custom {
+				id: emoji_id(606565381288493077),
+				name: None
+			},
+			message_id: server_role_message,
+			role_id: role_id(839970021803556905)
+		}),
+
+		// 2012
+		Box::new(ReactionRole {
+			channel_id,
+			guild_id,
+			emoji: RequestReactionType::Custom {
+				id: emoji_id(824789163605098516),
+				name: None
+			},
+			message_id: year_role_message,
+			role_id: role_id(628136070642401280)
+		}),
+
+		// 2013
+		Box::new(ReactionRole {
+			channel_id,
+			guild_id,
+			emoji: RequestReactionType::Custom {
+				id: emoji_id(824789178448871454),
+				name: None
+			},
+			message_id: year_role_message,
+			role_id: role_id(628136127643123732)
+		}),
+
+		// 2014
+		Box::new(ReactionRole {
+			channel_id,
+			guild_id,
+			emoji: RequestReactionType::Custom {
+				id: emoji_id(824789187956965386),
+				name: None
+			},
+			message_id: year_role_message,
+			role_id: role_id(628136132701454346)
+		}),
+
+		// 2015
+		Box::new(ReactionRole {
+			channel_id,
+			guild_id,
+			emoji: RequestReactionType::Custom {
+				id: emoji_id(824789203673808936),
+				name: None
+			},
+			message_id: year_role_message,
+			role_id: role_id(628136394023370752)
+		}),
+
+		// 2016
+		Box::new(ReactionRole {
+			channel_id,
+			guild_id,
+			emoji: RequestReactionType::Custom {
+				id: emoji_id(824789214251057182),
+				name: None
+			},
+			message_id: year_role_message,
+			role_id: role_id(628136665721864202)
+		}),
+
+		// 2017
+		Box::new(ReactionRole {
+			channel_id,
+			guild_id,
+			emoji: RequestReactionType::Custom {
+				id: emoji_id(824789229765656576),
+				name: None
+			},
+			message_id: year_role_message,
+			role_id: role_id(628137097244442627)
+		}),
+
+		// 2018
+		Box::new(ReactionRole {
+			channel_id,
+			guild_id,
+			emoji: RequestReactionType::Custom {
+				id: emoji_id(824789240737955880),
+				name: None
+			},
+			message_id: year_role_message,
+			role_id: role_id(628136798299750421)
+		}),
+
+		// 2019
+		Box::new(ReactionRole {
+			channel_id,
+			guild_id,
+			emoji: RequestReactionType::Custom {
+				id: emoji_id(824789250582380554),
+				name: None
+			},
+			message_id: year_role_message,
+			role_id: role_id(628136797095854100)
+		})
+	]
 }
 
 #[inline]
