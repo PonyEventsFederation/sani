1aa877d814209ff5a6d75a1306b3300c826b1a75
Meadowsys <blazeykirin@gmail.com>
2023-05-01 14:23:11 -0700

    get ids set up?

--- src/d.js/ids.ts
+++ src/d.js/ids.ts
@@ -24,20 +24,17 @@ export const galaconid: string = "602434888880095242";
 export const loyaltyrole = "865372668618735636";
 
 const channelid = "823351439279259648";
-const serverrolemessage = "839966718071406652";
-const yearrolemessage = "839966718507221013";
-const ee_yearrole_message = "// TODO";
+const message_1 = "1102705205822377985"; // https://discord.com/channels/602434888880095242/823351439279259648/1102705205822377985
+const message_2 = "1102705210503209051"; // https://discord.com/channels/602434888880095242/823351439279259648/1102705210503209051
+const message_3 = "1102705213351137341"; // https://discord.com/channels/602434888880095242/823351439279259648/1102705213351137341
+const message_4 = "1102705215553163324"; // https://discord.com/channels/602434888880095242/823351439279259648/1102705215553163324
+const message_5 = "1102705218052968559"; // https://discord.com/channels/602434888880095242/823351439279259648/1102705218052968559
+const message_6 = "1102705219743256656"; // https://discord.com/channels/602434888880095242/823351439279259648/1102705219743256656
+const message_7 = "1102705221098016798"; // https://discord.com/channels/602434888880095242/823351439279259648/1102705221098016798
+const message_8 = "1102705222813491221"; // https://discord.com/channels/602434888880095242/823351439279259648/1102705222813491221
+const message_9 = "1102705247782187150"; // https://discord.com/channels/602434888880095242/823351439279259648/1102705247782187150
+const message_10 = "1102705256162402485"; // https://discord.com/channels/602434888880095242/823351439279259648/1102705256162402485
 
-const message_1 = "// TODO";
-const message_2 = "// TODO";
-const message_3 = "// TODO";
-const message_4 = "// TODO";
-const message_5 = "// TODO";
-const message_6 = "// TODO";
-const message_7 = "// TODO";
-const message_8 = "// TODO";
-const message_9 = "// TODO";
-const message_10 = "// TODO";
 
 const emoji_gc_artist = "823502121487958016";
 const emoji_gc_dj = "823503291027161128";
@@ -65,49 +62,49 @@ const emoji_25 = "1102150462414803009";
 export const artistrole: ReactionRole = {
    channelid,
    emoji: emoji_gc_artist,
-   messageid: serverrolemessage,
+   messageid: message_1,
    roleid: "605726501924765706"
 };
 export const musicianrole: ReactionRole = {
    channelid,
    emoji: emoji_gc_dj,
-   messageid: serverrolemessage,
+   messageid: message_1,
    roleid: "605451090430918752"
 };
 export const cosplayerrole: ReactionRole = {
    channelid,
    emoji: emoji_gc_canniinnkeep,
-   messageid: serverrolemessage,
+   messageid: message_1,
    roleid: "607227135756599326"
 };
 export const memerole: ReactionRole = {
    channelid,
    emoji: emoji_gc_cannidab,
-   messageid: serverrolemessage,
+   messageid: message_1,
    roleid: "605454893620396060"
 };
 export const rprole: ReactionRole = {
    channelid,
    emoji: emoji_gc_cannisalute,
-   messageid: serverrolemessage,
+   messageid: message_1,
    roleid: "755487852720291851"
 };
 export const nobottagsrole: ReactionRole = {
    channelid,
    emoji: emoji_gc_stop,
-   messageid: serverrolemessage,
+   messageid: message_1,
    roleid: "962689871305121813"
 };
 export const movienightrole: ReactionRole = {
    channelid,
    emoji: emoji_gc_cannibizaam,
-   messageid: serverrolemessage,
+   messageid: message_1,
    roleid: "839970021803556905"
 };
 export const newsrole: ReactionRole = {
    channelid,
    emoji: emoji_gc_canniscroll,
-   messageid: serverrolemessage,
+   messageid: message_1,
    roleid: "784873956363599912"
 };
 
@@ -115,55 +112,55 @@ export const newsrole: ReactionRole = {
 export const gala_year2012: ReactionRole = {
    channelid,
    emoji: emoji_12,
-   messageid: yearrolemessage,
+   messageid: message_2,
    roleid: "628136070642401280"
 };
 export const gala_year2013: ReactionRole = {
    channelid,
    emoji: emoji_13,
-   messageid: yearrolemessage,
+   messageid: message_2,
    roleid: "628136127643123732"
 };
 export const gala_year2014: ReactionRole = {
    channelid,
    emoji: emoji_14,
-   messageid: yearrolemessage,
+   messageid: message_2,
    roleid: "628136132701454346"
 };
 export const gala_year2015: ReactionRole = {
    channelid,
    emoji: emoji_15,
-   messageid: yearrolemessage,
+   messageid: message_2,
    roleid: "628136394023370752"
 };
 export const gala_year2016: ReactionRole = {
    channelid,
    emoji: emoji_16,
-   messageid: yearrolemessage,
+   messageid: message_2,
    roleid: "628136665721864202"
 };
 export const gala_year2017: ReactionRole = {
    channelid,
    emoji: emoji_17,
-   messageid: yearrolemessage,
+   messageid: message_2,
    roleid: "628137097244442627"
 };
 export const gala_year2018: ReactionRole = {
    channelid,
    emoji: emoji_18,
-   messageid: yearrolemessage,
+   messageid: message_2,
    roleid: "628136798299750421"
 };
 export const gala_year2019: ReactionRole = {
    channelid,
    emoji: emoji_19,
-   messageid: yearrolemessage,
+   messageid: message_2,
    roleid: "628136797095854100"
 };
 export const gala_year2022: ReactionRole = {
    channelid,
    emoji: emoji_22,
-   messageid: yearrolemessage,
+   messageid: message_2,
    roleid: "1001441291281891359"
 };
 
@@ -171,24 +168,24 @@ export const gala_year2022: ReactionRole = {
 export const ee_year_2018: ReactionRole = {
    channelid,
    emoji: emoji_18,
-   messageid: ee_yearrole_message,
+   messageid: message_3,
    roleid: "1101264862711136276"
 };
 export const ee_year_2019: ReactionRole = {
    channelid,
    emoji: emoji_19,
-   messageid: ee_yearrole_message,
+   messageid: message_3,
    roleid: "1101264895648997386"
 };
 export const ee_year_2021: ReactionRole = {
    channelid,
    emoji: emoji_21,
-   messageid: ee_yearrole_message,
+   messageid: message_3,
    roleid: "1101264943191437443"
 };
 export const ee_year_2023: ReactionRole = {
    channelid,
    emoji: emoji_23,
-   messageid: ee_yearrole_message,
+   messageid: message_3,
    roleid: "1101265440161931265"
 };
